bd15a8d6e7ab839c61e82d2b3723e364b1ca5d09
Azie Melza Pratama <aziemp66@gmail.com>
2025-02-01 01:57:36 +0700

    feat : create entity

---
+++ internal/modules/loan/model/entity.go
@@ -0,0 +1,17 @@
+package loan_model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Loan struct {
+	ID                     uuid.UUID `db:"id"`
+	UserID                 uuid.UUID `db:"user_id"`
+	Principal              float64   `db:"pricipal"`
+	InterestRatePercentage float64   `db:"interest_rate_percentage"`
+	WeeklyPayment          int       `db:"weekly_payment"`
+	PaidWeeks              int       `db:"paid_weeks"`
+	LoanStartDate          time.Time `db:"loan_start_date"`
+}
